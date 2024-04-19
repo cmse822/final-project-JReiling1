b455ee13763744d7529d8910a437002ab781ddd4
JReiling1 <reiling1@msu.edu>
2024-04-19 16:16:23 -0400

    Data for openMP

--- dijkstra_openMP.c
+++ dijkstra_openMP.c
@@ -1,6 +1,7 @@
 #include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <omp.h>
 #include <time.h>
 
@@ -10,8 +11,8 @@
 // This version is openMP implimentation. See other verions of Dijkstra for parallelized
 // verions in both MPI and openMP
 
-#define num_verticies 9
-#define NUM_THREADS 2
+#define num_verticies 1000
+#define NUM_THREADS 64
 
 
 // Notice that we're instantiating omp to avoid race conditions
@@ -31,7 +32,7 @@ int minDistance(int shortest_dist[], bool short_path_tree[]) {
 }
 
 
-void printSolution(FILE *outputFile, int dist[], int source)
+void printSolution(FILE *outputFile, int dist[], int source, double run_time)
 {
     #pragma omp critical
     {
@@ -40,25 +41,66 @@ void printSolution(FILE *outputFile, int dist[], int source)
         for (int i = 0; i < num_verticies; i++){ 
             printf("%d \t\t\t\t %d\n", i, dist[i]);
         }
-        fprintf(outputFile, "%d, %d, %f\n", source, omp_get_thread_num());
+        fprintf(outputFile, "%d, %d, %f\n", num_verticies, omp_get_thread_num(), run_time);
             
     }
 }
 
+void write_to_csv(FILE *outputFile, double run_time)
+{
+    #pragma omp critical
+    {
+        fprintf(outputFile, "%d, %d, %f\n", num_verticies, NUM_THREADS, run_time);
+            
+    }
+}
+
+int** generateGraph() {
+    int** graph = (int**)malloc(num_verticies * sizeof(int*));
+    if (graph == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    
+    for (int i = 0; i < num_verticies; i++) {
+        graph[i] = (int*)malloc(num_verticies * sizeof(int));
+        if (graph[i] == NULL) {
+            printf("Memory allocation failed.\n");
+            exit(1);
+        }
+    }
+
+    // Generating random weights for the edges
+    for (int i = 0; i < num_verticies; i++) {
+        for (int j = 0; j < num_verticies; j++) {
+            if (i == j) {
+                graph[i][j] = 0;  // Diagonal elements set to 0
+            } else {
+                int seed = 123;
+                srand(seed);
+                graph[i][j] = rand() % 100;  // Random weight between 0 and 19
+            }
+        }
+    }
+
+    return graph;
+}
 
 int main() {
    /* Example graph found online */
-    int graph[num_verticies][num_verticies] = { 
-        { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-        { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-        { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-        { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-        { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-        { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-        { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-        { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-        { 0, 0, 2, 0, 0, 0, 6, 7, 0 } 
-    };
+    // int graph[num_verticies][num_verticies] = { 
+    //     { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+    //     { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+    //     { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+    //     { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+    //     { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+    //     { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+    //     { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+    //     { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+    //     { 0, 0, 2, 0, 0, 0, 6, 7, 0 } 
+    // };
+
+    int** graph = generateGraph();
 
     // Initialize csv 
     const char *csv_file_name="dijkstra_openMP.csv";
@@ -76,7 +118,7 @@ int main() {
     fseek(outputFile, 0, SEEK_END); // Move to the end of the file
     long fileSize = ftell(outputFile); // Get the current position (file size)
     if (fileSize == 0) { // Check if the file is empty
-        fprintf(outputFile, "Source Node, Thread Num, Runtime\n");
+        fprintf(outputFile, "Num Verticies, Num Ranks, Runtime\n");
     }
 
 
@@ -124,12 +166,14 @@ int main() {
                 }
             }   
 
-            // Print resulting information
-            printSolution(outputFile, shortest_dist, source);
+            // // Print resulting information
+            // printSolution(outputFile, shortest_dist, source);
         }
     }
-        double end_time = omp_get_wtime();
-        double run_time = end_time - start_time;
+    double end_time = omp_get_wtime();
+    double run_time = end_time - start_time;
+    // Print resulting information
+    write_to_csv(outputFile, run_time);
 
 
     fclose(outputFile);
